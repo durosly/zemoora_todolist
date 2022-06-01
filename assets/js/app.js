1bbdd136db75e7f1273cd77acab698bcd4ded8d5
durosly <slyboydon1@gmail.com>
2022-06-01 12:41:44 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// TODO: get a reference to the form element
+
+// TODO: attach an event listener (type=submit)
+
+    // TODO: prevent the default behaviour
+
+    // TODO: get input item value
+
+    // TODO: perform simple validation on input value
+
+    // TODO: if data is valid
+
+        // TODO: add data to todo list
+        
+    // TODO: else
+
+        // TODO: alert an error message
